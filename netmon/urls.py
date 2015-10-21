065fe417b2a214f7ffca3835ba8114db8a55dd61
Thomas Maier <tmaier@fs.cs.hm.edu>
2015-10-21 20:26:01 +0200

    Added dashboard view

--- netmon/urls.py
+++ netmon/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import DeviceStatusDetail, DeviceStatusList
+from .views import DeviceStatusDetail, DeviceStatusList, DashboardView
 
 urlpatterns = [
     url(r'^deviceStatus/$', DeviceStatusList.as_view()),
     url(r'^deviceStatus/(?P<pk>[0-9]+)/$', DeviceStatusDetail.as_view()),
+    url(r'^dashboard/$', DashboardView.as_view()),
 ]
--- netmon/views.py
+++ netmon/views.py
@@ -1,6 +1,17 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from .serializers import DeviceStatusSerializer
-from .models import DeviceStatus
+from .models import User, DeviceType, Device, DeviceStatus
+from django.views.generic import TemplateView
+from django.http import HttpResponse
+
+
+class DashboardView(TemplateView):
+    template_name = 'dashboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(TemplateView, self).get_context_data(**kwargs)
+        context['status_objects'] = DeviceStatus.objects.all()
+        return context
 
 
 class DeviceStatusList(ListCreateAPIView):
