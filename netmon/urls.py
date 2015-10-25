8443a14ad5af1ab99455761ec60cd245a710ef54
Thomas Maier <tmaier@fs.cs.hm.edu>
2015-10-25 19:08:37 +0100

    Improved templates.

--- netmon/urls.py
+++ netmon/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from .views import DeviceStatusDetail, DeviceStatusList, DashboardView
 
 urlpatterns = [
-    url(r'^api/deviceStatus/$', DeviceStatusList.as_view(), name='device_status'),
-    url(r'^api/deviceStatus/(?P<pk>[0-9]+)/$', DeviceStatusDetail.as_view()),
-    url(r'^dashboard/$', DashboardView.as_view()),
+    url(r'^api/deviceStatus/$', DeviceStatusList.as_view(), name='api_device_status_list'),
+    url(r'^api/deviceStatus/(?P<pk>[0-9]+)/$', DeviceStatusDetail.as_view(), name='api_device_status_detail'),
+    url(r'^dashboard/$', DashboardView.as_view(), name='dashboard'),
 ]
